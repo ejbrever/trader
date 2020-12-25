fc4411689c4516bb65d93b2965a29c9e2e20503d
Eric Breverman <ejbrever@gmail.com>
2020-12-24 21:07:09 -0800

    updates

---
+++ one/database/database.go
@@ -0,0 +1,113 @@
+// Package database manages the databases for trader-one.
+package database
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/alpacahq/alpaca-trade-api-go/alpaca"
+	"github.com/ejbrever/trader/one/purchase"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	username = "one"
+	password = "password"
+	hostname = "127.0.0.1:3306"
+	dbName   = "one"
+)
+
+// Client manages interactions with the database.
+type Client struct {
+	db *sql.DB
+}
+
+// New creates a new database client that is connected to the database.
+func New() (*Client, error) {
+	db, err := open()
+	return &Client{
+		db: db,
+	}, err
+}
+
+// Insert inserts purchase data into the table.
+func (c *Client) Insert(p *purchase.Purchase) error {
+	buyBytes, err := json.Marshal(p.BuyOrder)
+	if err != nil {
+		return fmt.Errorf("unable to marshal buy order: %v", err)
+	}
+
+	sellBytes, err := json.Marshal(p.SellOrder)
+	if err != nil {
+		return fmt.Errorf("unable to marshal sell order: %v", err)
+	}
+
+	query := `INSERT INTO trader_one(buy_order, sell_order) VALUES (?, ?)`
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	stmt, err := c.db.PrepareContext(ctx, query)
+	if err != nil {
+		return fmt.Errorf("unable to prepare SQL statement: %v", err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(ctx, string(buyBytes), string(sellBytes))
+	if err != nil {
+		return fmt.Errorf("unable to insert row: %v", err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return fmt.Errorf("unable to find new ID: %v", err)
+	}
+	p.ID = id
+	return nil
+}
+
+// Purchases retrieves all purchases stored in the database.
+// TODO(ejbrever): Argument to filter by date(s).
+func (c *Client) Purchases() ([]*purchase.Purchase, error) {
+	results, err := c.db.Query(`SELECT id, buy_order, sell_order FROM trader_one`)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get purchases from table: %v", err)
+	}
+
+	var purchases []*purchase.Purchase
+	for results.Next() {
+		var id int64
+		var buyOrderJSON, sellOrderJSON string
+		err = results.Scan(&id, &buyOrderJSON, &sellOrderJSON)
+		if err != nil {
+			return nil, fmt.Errorf("unable to scan row: %v", err)
+		}
+		var buyOrder, sellOrder *alpaca.Order
+		if err = json.Unmarshal([]byte(buyOrderJSON), buyOrder); err != nil {
+			return nil, fmt.Errorf("unable to unmarshal %q: %v", buyOrderJSON, err)
+		}
+		if err = json.Unmarshal([]byte(sellOrderJSON), sellOrder); err != nil {
+			return nil, fmt.Errorf("unable to unmarshal %q: %v", sellOrderJSON, err)
+		}
+		purchases = append(purchases, &purchase.Purchase{
+			ID:        id,
+			BuyOrder:  buyOrder,
+			SellOrder: sellOrder,
+		})
+	}
+	return purchases, nil
+}
+
+// open opens the database.
+func open() (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn(dbName))
+	if err != nil {
+		return nil, fmt.Errorf("unable to open database %q: %v", dbName, err)
+	}
+	return db, nil
+}
+
+func dsn(dbName string) string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
+}
---
+++ one/database/go.mod
@@ -0,0 +1,9 @@
+module database.go
+
+go 1.15
+
+require (
+	github.com/alpacahq/alpaca-trade-api-go v1.7.0 // indirect
+	github.com/ejbrever/trader/one/purchase v0.0.0-20201225041924-4f7f3e90111a // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+)
