e03bd2f547b163e5c3667781df64f03524169657
Eric Breverman <ejbrever@gmail.com>
2020-12-28 07:52:49 -0800

    Debug logs.

--- one/database/database.go
+++ one/database/database.go
@@ -52,30 +52,39 @@ func (c *MySQLClient) Insert(p *purchase.Purchase) error {
 	if err != nil {
 		return fmt.Errorf("unable to marshal buy order: %v", err)
 	}
+	fmt.Printf("1")
 
 	sellBytes, err := json.Marshal(p.SellOrder)
 	if err != nil {
 		return fmt.Errorf("unable to marshal sell order: %v", err)
 	}
+	fmt.Printf("2")
 
 	query := `INSERT INTO trader_one(buy_order, sell_order) VALUES (?, ?)`
 	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	fmt.Printf("3")
 	defer cancelFunc()
 	stmt, err := c.db.PrepareContext(ctx, query)
+	fmt.Printf("4")
 	if err != nil {
 		return fmt.Errorf("unable to prepare SQL statement: %v", err)
 	}
 	defer stmt.Close()
+	fmt.Printf("5")
 
 	res, err := stmt.ExecContext(ctx, string(buyBytes), string(sellBytes))
+	fmt.Printf("6")
 	if err != nil {
 		return fmt.Errorf("unable to insert row: %v", err)
 	}
 	id, err := res.LastInsertId()
+	fmt.Printf("7")
 	if err != nil {
 		return fmt.Errorf("unable to find new ID: %v", err)
 	}
+	fmt.Printf("8")
 	p.ID = id
+	fmt.Printf("9")
 	return nil
 }
 
